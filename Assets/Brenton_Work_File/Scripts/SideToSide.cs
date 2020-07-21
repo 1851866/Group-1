eeb19c308a54c210f1daf110e38258d96a12f34a
BrentonBudler <60913244+BrentonBudler@users.noreply.github.com>
2020-07-21 20:46:27 +0200

    Procedural Generation

---
+++ Assets/Brenton_Work_File/Scripts/SideToSide.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideToSide : MonoBehaviour
+{
+    public bool moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving == false)
+        {
+            StartCoroutine(Move());
+        }
+    }
+
+
+    IEnumerator Move()
+    {
+        moving = true;
+        for (int i = 0; i < 200; i++)
+        {
+            transform.Translate(0.01f, 0, 0);
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        for (int i = 0; i < 400; i++)
+        {
+            transform.Translate(-0.01f, 0, 0);
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        for (int i = 0; i < 200; i++)
+        {
+            transform.Translate(0.01f, 0, 0);
+            yield return new WaitForSeconds(0.001f);
+        }
+        moving = false;
+
+    }
+}
