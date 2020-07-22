e6637218ada0a185ae6283a180470b7a35bcf3a7
nadia-hassim <64084088+nadia-hassim@users.noreply.github.com>
2020-07-22 15:44:03 +0200

    made buttons

---
+++ Assets/Nadia_Work_File/Scripts/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("AttemptTwo");
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+        
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu");
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void HowTo()
+    {
+        SceneManager.LoadScene("How To");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
